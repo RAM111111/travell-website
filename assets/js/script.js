692508e554992b0ed993be2f85861433af38ea65
RAM111111 <r.r.99@windowslive.com>
2021-08-20 19:42:57 +0300

    home section

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const navmenu = document.getElementById("nav_menu"),
+      navtoggle = document.getElementById("nav_toggle"),
+      navclose = document.getElementById("nav_close")
+
+
+      if(navtoggle){
+          navtoggle.addEventListener('click',() => {
+              navmenu.classList.add('show_menu')
+          })
+      }
+
+      if(navclose){
+        navclose.addEventListener('click',() => {
+            navmenu.classList.remove('show_menu')
+        })
+    }
+
+
+    function scrollheader(){
+        const header = document.getElementById('header');
+        if(this.scrollY >= 100)header.classList.add('scroll_header');else header.classList.remove('scroll_header');
+    }
+    window.addEventListener('scroll',scrollheader);
